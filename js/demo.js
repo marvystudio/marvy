88a0ecc42bcb457665ce36860e705f8914cdf330
khang <khangbg17@gmail.com>
2019-06-11 19:05:13 +0700

    asc

--- js/demo.js
+++ js/demo.js
@@ -15,3 +15,22 @@ $(function () {
 
 });
 
+$(window).scroll(function() {
+
+    var wScroll = $(this).scrollTop();
+    $('.eco-nike').css({
+        'transform' : 'translateX(-'+ wScroll /4 +'%)'
+    });
+    $('.eco-airforce').css({
+        'transform' : 'translateX('+ wScroll /4 +'%)'
+    });
+    if(wScroll > $('.eco-browse-all').offset().top - $(window).height()){
+        $('.eco-browse-all').css({'background-position':'center '+ (wScroll - $('.eco-browse-all').offset().top) +'px'});
+
+        var opacity = (wScroll - $('.eco-browse-all').offset().top + 400) / (wScroll / 5);
+
+        $('.eco-window-tint').css({'opacity': opacity});
+
+    }
+});
+
